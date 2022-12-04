d07fed62c1c5f58dca08468758ce1d7d647a2b86
PJJ <puayjianjun@gmail.com>
2022-12-04 23:57:52 +0800

    completed Day4 Part2

---
+++ code/2022/src/Day4p2.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day4p2 {
+  public static void main(String[] args) throws Exception {
+    List<String[]> myList = new ArrayList<>();
+
+    // count returns the answer
+    int count = 0;
+    try {
+        File myFile = new File("./code/2022/inputs/day4.txt");
+        Scanner myReader = new Scanner(myFile);
+        
+        while(myReader.hasNextLine()) {
+            String[] splitLine = myReader.nextLine().split("[-,]", 5);
+
+            myList.add(splitLine);
+        }
+        myReader.close();
+    } catch (Exception ex) {
+        System.out.println("An error occured");
+        ex.printStackTrace();
+    }
+
+    for(int i=0; i<myList.size(); i++) {
+        if(Integer.parseInt(myList.get(i)[0]) <=  Integer.parseInt(myList.get(i)[2])) {
+            if(Integer.parseInt(myList.get(i)[1]) >=  Integer.parseInt(myList.get(i)[2])) { count++; }
+        } else if (Integer.parseInt(myList.get(i)[0]) <=  Integer.parseInt(myList.get(i)[3])) { count++; }
+        
+    } 
+
+    System.out.println(count);
+}
+}
