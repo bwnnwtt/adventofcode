b9aecb5ee07cc7e100398321a5d15f5b637dedd9
PJJ <puayjianjun@gmail.com>
2022-12-05 23:53:09 +0800

    removed unused imports

--- code/2022/src/Day5p2.java
+++ code/2022/src/Day5p2.java
@@ -1,8 +1,6 @@
 import java.io.File;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 
